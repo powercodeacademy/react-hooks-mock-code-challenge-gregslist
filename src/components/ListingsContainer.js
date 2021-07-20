bc0a9f3c62beb8a832a2ac346f0502c1accc3a0e
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-07-20 13:39:14 -0400

    First deliverable done

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings }) {
+  const listingCards = listings.map(listing => (
+    <ListingCard
+        key={listing.id}
+        listing={listing}
+    />
+  ))
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
